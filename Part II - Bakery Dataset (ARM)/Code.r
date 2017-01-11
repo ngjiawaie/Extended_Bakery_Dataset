8e1db8f53e3de950ff33947720d59cdd5c5f2610
Lee Bing Yuan <johnlee961010@gmail.com>
2017-01-11 19:39:14 +0800

    Converted csv to transactional file.
    File ready to run on apriori.

--- Part II - Bakery Dataset (ARM)/Code.r
+++ Part II - Bakery Dataset (ARM)/Code.r
@@ -21,3 +21,8 @@ sparse_df$Food_Num_4 <- df$food[match(sparse_df$Food_Num_4,df$id)]
 sparse_df$Food_Num_5 <- df$food[match(sparse_df$Food_Num_5,df$id)]
 sparse_df$Food_Num_6 <- df$food[match(sparse_df$Food_Num_6,df$id)]
 names(full_binary_df) <- c("Receipt_Number", as.character(df$food))
+#------------------------------------------------------------------End of Data Preparation
+#-----------Converting csv files to transaction/ market basket format for apriori analysis
+write.csv(receipt_df, "output.csv", row.names = F)
+required_Data<-read.transactions("output.csv", rm.duplicates=TRUE, format = "basket")
+#----------reuired_Data should be ready for apriori by now.
